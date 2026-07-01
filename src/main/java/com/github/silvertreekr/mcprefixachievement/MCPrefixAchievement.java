a4cadf4257965f8fc98848eec5165ec0d0bec46c
SilverTreeKR <75466911+SilverTree-KR@users.noreply.github.com>
2026-07-01 23:42:44 +0900

    Initial Commit

---
+++ src/main/java/com/github/silvertreekr/mcprefixachievement/MCPrefixAchievement.java
@@ -0,0 +1,14 @@
+package com.github.silvertreekr.mcprefixachievement;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCPrefixAchievement extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
