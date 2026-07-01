0ea37e5d47083eace3dc8b9fd926a5d017bc6d6e
SilverTreeKR <75466911+SilverTree-KR@users.noreply.github.com>
2026-07-01 23:46:24 +0900

    feat: main클래스에 DB initailize

--- src/main/java/com/github/silvertreekr/mcprefixachievement/MCPrefixAchievement.java
+++ src/main/java/com/github/silvertreekr/mcprefixachievement/MCPrefixAchievement.java
@@ -1,9 +1,28 @@
 package com.github.silvertreekr.mcprefixachievement;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.silvertreekr.mcprefixachievement.database.MysqlDatabase;
+import org.jetbrains.annotations.NotNull;
+
 public final class MCPrefixAchievement extends JavaPlugin {
+    private static @NotNull MysqlDatabase mysqlDatabase;
+
+    public static @NotNull MysqlDatabase getMysqlDatabase() {
+        return mysqlDatabase;
+    }
+
     @Override
     public void onEnable() {
+
+        saveDefaultConfig();
+        reloadConfig();
+        try {
+            mysqlDatabase = MysqlDatabase.initialize(this);
+        } catch (Exception e) {
+            getSLF4JLogger().error("Failed to initialize MySQL database", e);
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
 
     }
 
