87564f41fe543ee5243f88957874ec3a9296dc85
SilverTreeKR <75466911+SilverTree-KR@users.noreply.github.com>
2026-07-05 03:36:02 +0900

    Feat: 가정파괴범 칭호 추가

---
+++ src/main/java/com/github/silvertreekr/mcprefixachievement/listener/EntityPickupItemEventListener.java
@@ -0,0 +1,59 @@
+package com.github.silvertreekr.mcprefixachievement.listener;
+
+import com.github.silvertreekr.mcprefixachievement.MCPrefixAchievement;
+import com.github.silvertreekr.mcprefixachievement.config.PrefixConfigManager;
+import com.github.silvertreekr.mcprefixachievement.dao.UserPrefixManager;
+import com.github.silvertreekr.mcprefixachievement.dao.UserStatsManager;
+import com.github.silvertreekr.mcprefixachievement.model.Prefix;
+import com.github.silvertreekr.mcprefixachievement.model.PrefixStat;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.UUID;
+
+public class EntityPickupItemEventListener implements Listener {
+    private final MCPrefixAchievement plugin = MCPrefixAchievement.getInstance();
+    private final UserPrefixManager prefixManager = plugin.getUserPrefixManager();
+    private final UserStatsManager statsManager = plugin.getUserStatsManager();
+    private final PrefixConfigManager prefixConfigManager = plugin.getPrefixConfigManager();
+
+    private void grantPrefix(UUID uuid, int prefixID, Player player) {
+        if (prefixID != -1) {
+            Prefix prefix = prefixConfigManager.getPrefixById(prefixID);
+
+            if (prefix != null) {
+                prefixManager.addPrefix(uuid, prefixID);
+                player.sendRichMessage("<bold>[ 칭호 시스템 ] <reset>축하합니다 ! <prefix><reset>을 획득하셨습니다 !", Placeholder.component("prefix", prefix.getDisplayPrefix()));
+            }
+        }
+    }
+
+    public EntityPickupItemEventListener(JavaPlugin plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler
+    public void onPlayerPickUpDragonEgg(EntityPickupItemEvent event) {
+        if (event.getEntity() instanceof Player player){
+            UUID uuid = event.getEntity().getUniqueId();
+            int prefixID = -1;
+
+            if (event.getItem().getItemStack().getType().equals(Material.DRAGON_EGG)) {
+                int count = statsManager.getStatValue(uuid, PrefixStat.GET_DRAGON_EGG);
+                count++;
+                statsManager.setStatValue(uuid, PrefixStat.GET_DRAGON_EGG, count);
+                if (count == 1) {
+                    prefixID = 12;
+                    player.give(new ItemStack(Material.ENCHANTED_GOLDEN_APPLE, 1));
+                }
+            }
+            grantPrefix(uuid, 12, player);
+        }
+    }
+}
