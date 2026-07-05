e1770f078707b1b6b7b69804c0a6e5a0ebf483f9
SilverTreeKR <75466911+SilverTree-KR@users.noreply.github.com>
2026-07-05 23:56:00 +0900

    feat: broadcastPrefix() 메서드 추가

--- src/main/java/com/github/silvertreekr/mcprefixachievement/util/PrefixGranter.java
+++ src/main/java/com/github/silvertreekr/mcprefixachievement/util/PrefixGranter.java
@@ -4,7 +4,10 @@ import com.github.silvertreekr.mcprefixachievement.MCPrefixAchievement;
 import com.github.silvertreekr.mcprefixachievement.config.PrefixConfigManager;
 import com.github.silvertreekr.mcprefixachievement.dao.UserPrefixManager;
 import com.github.silvertreekr.mcprefixachievement.model.Prefix;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
@@ -31,5 +34,27 @@ public class PrefixGranter {
         prefixManager.addPrefix(uuid, prefixID);
         player.sendRichMessage("<bold>[ 칭호 시스템 ] <reset>축하합니다 ! <prefix><reset>을 획득하셨습니다 !", Placeholder.component("prefix", prefix.getDisplayPrefix()));
         player.playSound(player.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 1.0f, 1.0f);
+    }
+
+    public static void broadcastPrefix(Player player, int prefixID) {
+        MCPrefixAchievement plugin = MCPrefixAchievement.getInstance();
+        PrefixConfigManager prefixConfigManager = plugin.getPrefixConfigManager();
+        Prefix prefix = prefixConfigManager.getPrefixById(prefixID);
+
+        if (prefix == null) {
+            return;
+        }
+
+        if (prefixID == -1) {
+            return;
+        }
+
+        Component playerName = Component.text(player.getName());
+        Component message = MiniMessage.miniMessage().deserialize(
+                "<bold>[ 칭호 시스템 ] <reset><bold><green><player><reset>님께서 <prefix><reset>을 획득하셨습니다 !"
+                ,Placeholder.component("player", playerName)
+                ,Placeholder.component("prefix", prefix.getDisplayPrefix())
+        );
+        Bukkit.broadcast(message);
     }
 }
