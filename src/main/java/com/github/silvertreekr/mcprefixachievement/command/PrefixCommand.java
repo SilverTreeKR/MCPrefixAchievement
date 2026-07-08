136b010a33ecc952ef409fe6fbbca8c1bfb6f576
SilverTreeKR <75466911+SilverTree-KR@users.noreply.github.com>
2026-07-08 17:57:20 +0900

    feat: 사용 실패 시 사운드 추가 및 칭호 지급 도움말 추가

--- src/main/java/com/github/silvertreekr/mcprefixachievement/command/PrefixCommand.java
+++ src/main/java/com/github/silvertreekr/mcprefixachievement/command/PrefixCommand.java
@@ -5,6 +5,7 @@ import com.github.silvertreekr.mcprefixachievement.config.PrefixConfigManager;
 import com.github.silvertreekr.mcprefixachievement.model.Prefix;
 import com.github.silvertreekr.mcprefixachievement.model.PrefixName;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import org.bukkit.Sound;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
 import org.bukkit.entity.Player;
@@ -24,18 +25,20 @@ public class PrefixCommand extends BukkitCommand {
     public boolean execute(@NotNull CommandSender sender, @NotNull String label, @NotNull String @NotNull [] args) {
         // /칭호 정보 [칭호ID] -> 특정 칭호의 달성 조건을 보는 커맨드
         // /칭호 목록 [페이지] -> 칭호들의 목록을 보는 커맨드 (15개씩 끊어서 페이지)
+        // /칭호 지급 [대상 플레이어] [칭호ID] -> 대상 플레이어에게 칭호를 지급하는 커맨드
         // /칭호 -> 칭호 명령어들 반환
         if (args.length == 0) {
-            sender.sendRichMessage("<bold>[ 칭호 시스템 ] <reset>사용 가능한 명령어:");
-            sender.sendRichMessage("<bold>[ 칭호 시스템 ] <reset>/칭호 정보 [칭호ID] - 특정 칭호의 정보를 확인합니다.");
-            sender.sendRichMessage("<bold>[ 칭호 시스템 ] <reset>/칭호 목록 [페이지] - 칭호들의 목록을 확인합니다.");
-            sender.sendRichMessage("<bold>[ 칭호 시스템 ] <reset>/[칭호명] - 특정 칭호의 특수 효과를 부여받습니다.");
-            sender.sendRichMessage("<bold>[ 칭호 시스템 ] <reset>(공백 없이 입력)");
             sender.sendRichMessage("<bold>【 칭호 】 <reset>사용 가능한 명령어:");
             sender.sendRichMessage("<bold>【 칭호 】 <reset>/칭호 정보 [칭호ID] - 특정 칭호의 정보를 확인합니다.");
             sender.sendRichMessage("<bold>【 칭호 】 <reset>/칭호 목록 [페이지] - 칭호들의 목록을 확인합니다.");
             sender.sendRichMessage("<bold>【 칭호 】 <reset>/[칭호명] - 특정 칭호의 특수 효과를 부여받습니다.");
             sender.sendRichMessage("<bold>【 칭호 】 <reset>(공백 없이 입력)");
+            if (!sender.isOp()) {
+                return true;
+            }
+            sender.sendRichMessage("<bold>【 칭호 】 --------------------------------------------------------");
+            sender.sendRichMessage("<bold>【 칭호 】 <reset>/칭호 지급 [대상 플레이어] [칭호ID] ");
+            sender.sendRichMessage("<bold>【 칭호 】 <reset>- 대상 플레이어에게 칭호를 지급합니다.");
             return true;
         }
 
@@ -44,7 +47,11 @@ public class PrefixCommand extends BukkitCommand {
             case "정보" -> {
                 if (args.length == 1) {
                     sender.sendRichMessage("<bold>【 칭호 】 <reset>사용법: /칭호 정보 [칭호 ID]");
-                    return true;
+                    if (!(sender instanceof Player player)) {
+                        return false;
+                    }
+                    player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0f, 1.0f);
+                    return false;
                 }
                 if (args.length == 2) {
                     try {
@@ -70,6 +77,10 @@ public class PrefixCommand extends BukkitCommand {
                         }
                     } catch (NumberFormatException e) {
                         sender.sendRichMessage("<bold>【 칭호 】 <reset><red>올바르지 않은 칭호 ID입니다.");
+                        if (!(sender instanceof  Player player)) {
+                            return false;
+                        }
+                        player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0f, 1.0f);
                         return false;
                     }
                 }
@@ -92,6 +103,10 @@ public class PrefixCommand extends BukkitCommand {
                         int skip = (page - 1) * pageSize;
                         if (skip >= prefixMap.size()) {
                             sender.sendRichMessage("<bold>【 칭호 】 <reset><red>해당 페이지에는 칭호가 존재하지 않습니다.");
+                            if (!(sender instanceof Player player)) {
+                                return false;
+                            }
+                            player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0f, 1.0f);
                             return false;
                         }
                         int shown = 0;
@@ -105,6 +120,10 @@ public class PrefixCommand extends BukkitCommand {
                         return true;
                     } catch (NumberFormatException e) {
                         sender.sendRichMessage("<bold>【 칭호 】 <reset><red>올바르지 않은 페이지 번호입니다.");
+                        if (!(sender instanceof Player player)) {
+                            return false;
+                        }
+                        player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0f, 1.0f);
                         return false;
                     }
                 }
@@ -112,6 +131,10 @@ public class PrefixCommand extends BukkitCommand {
             default -> {
                 sender.sendRichMessage("<bold>【 칭호 】 <reset><red>올바르지 않은 사용법입니다.");
                 sender.sendRichMessage("<bold>【 칭호 】 <reset><red>올바른 사용법: /칭호 정보 [칭호명] 또는 /칭호 목록 [페이지]");
+                if (!(sender instanceof Player player)) {
+                    return false;
+                }
+                player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0f, 1.0f);
                 return false;
             }
         }
