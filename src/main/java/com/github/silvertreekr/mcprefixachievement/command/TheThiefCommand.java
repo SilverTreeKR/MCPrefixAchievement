85227018770bccdbf7f42f88aebf1071b03bad93
SilverTreeKR <75466911+SilverTreeKR@users.noreply.github.com>
2026-07-10 22:01:39 +0900

    feat: "도적" 전용 명령어

---
+++ src/main/java/com/github/silvertreekr/mcprefixachievement/command/TheThiefCommand.java
@@ -0,0 +1,69 @@
+package com.github.silvertreekr.mcprefixachievement.command;
+
+import com.github.silvertreekr.mcprefixachievement.MCPrefixAchievement;
+import com.github.silvertreekr.mcprefixachievement.model.Prefix;
+import com.github.silvertreekr.mcprefixachievement.model.PrefixName;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.defaults.BukkitCommand;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.UUID;
+
+public class TheThiefCommand extends BukkitCommand {
+    public TheThiefCommand(@NotNull JavaPlugin plugin) {
+        super("도적");
+        plugin.getServer().getCommandMap().register("mcprefixachievement", this);
+    }
+
+    private final HashMap<UUID, LocalDateTime> lastExecutions = new HashMap<>();
+    private final int COOLTIME = 180;
+
+    @Override
+    public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String @NotNull [] args) {
+        if (!(sender instanceof Player player)) {
+            return false;
+        }
+        if (!MCPrefixAchievement.getInstance().getUserPrefixManager().hasPrefix(player.getUniqueId(), PrefixName.THE_THIEF)) {
+            Prefix prefix = MCPrefixAchievement.getInstance().getPrefixConfigManager().getPrefixById(PrefixName.THE_THIEF);
+            if (prefix == null) {
+                return false;
+            }
+            sender.sendRichMessage("<bold>【 칭호 】 <reset><prefix><reset><red>를 가지고 있지 않습니다 !", Placeholder.component("prefix", prefix.getDisplayPrefix()));
+
+            return false;
+        }
+        if (args.length != 0) {
+            sender.sendRichMessage("<bold>【 칭호 】 <reset><red>올바르지 않은 명령어입니다.");
+            sender.sendRichMessage("<bold>【 칭호 】 <reset>올바른 사용법: /[칭호명]");
+
+            return false;
+        }
+        LocalDateTime nowTime = LocalDateTime.now();
+        if (lastExecutions.containsKey(player.getUniqueId())) {
+            LocalDateTime lastExecution = lastExecutions.get(player.getUniqueId());
+            Duration duration = Duration.between(lastExecution, nowTime);
+            long minutesPassed = duration.toMinutes();
+
+            if (minutesPassed < COOLTIME) {
+                long minutesLeft = COOLTIME - minutesPassed;
+
+                player.sendRichMessage("<bold>【 칭호 】 <reset>남은 시간: <bold><red><cooltime>분", Placeholder.unparsed("cooltime", String.valueOf(minutesLeft)));
+                return true;
+            }
+        }
+        lastExecutions.put(player.getUniqueId(), nowTime);
+
+        player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 20*60*10, 0));
+        player.sendRichMessage("<bold>【 칭호 】 <reset><aqua>신속 효과<reset>가 적용되었습니다 !");
+
+        return true;
+    }
+}
