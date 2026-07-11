b5b2ea44c5fe84b817c788d4941d4c44bfa48600
SilverTreeKR <75466911+SilverTree-KR@users.noreply.github.com>
2026-07-11 14:29:06 +0900

    fix: "발설금지" 명령어에서 칭호 인식을 "망치나가신다"로 하고 있는 문제

--- src/main/java/com/github/silvertreekr/mcprefixachievement/command/DontMakeSoundCommand.java
+++ src/main/java/com/github/silvertreekr/mcprefixachievement/command/DontMakeSoundCommand.java
@@ -33,8 +33,8 @@ public class DontMakeSoundCommand extends BukkitCommand {
         if (!(sender instanceof Player player)) {
             return false;
         }
-        if (!MCPrefixAchievement.getInstance().getUserPrefixManager().hasPrefix(player.getUniqueId(), PrefixName.HAMMER_ON)) {
-            Prefix prefix = MCPrefixAchievement.getInstance().getPrefixConfigManager().getPrefixById(PrefixName.HAMMER_ON);
+        if (!MCPrefixAchievement.getInstance().getUserPrefixManager().hasPrefix(player.getUniqueId(), PrefixName.DONT_MAKE_SOUND)) {
+            Prefix prefix = MCPrefixAchievement.getInstance().getPrefixConfigManager().getPrefixById(PrefixName.DONT_MAKE_SOUND);
             if (prefix == null) {
                 return false;
             }
